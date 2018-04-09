4e17a7871bceacb547fba31116512e003cf33379
NYAMUJOSEPH <jnjogu@cytonn.com>
2018-04-09 10:38:08 +0300

    Purchasing Concert Tickets - Faking the payment gateway

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentGateway;
 use App\Concert;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -7,9 +9,13 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PurchaseTicketsTest extends TestCase
 {
+    use DatabaseMigrations;
     /** @test */
     function customer_can_purchase_concert_tickets()
     {
+        $paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $paymentGateway);
+
         $concert = factory(Concert::class)->create([
             'ticket_price' => 3250
         ]);
@@ -19,6 +25,8 @@ class PurchaseTicketsTest extends TestCase
             'ticket_quantity' => 3,
             'payment_token' => $paymentGateway->getValidTestToken(),
         ]);
+
+        $this->assertResponseStatus(201);
 
         $this->assertEquals(9750, $paymentGateway->totalCharges());
 
